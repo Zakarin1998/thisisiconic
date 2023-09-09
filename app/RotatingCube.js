d9c58fcc2de3c4b4ab4af8040dfbf0f90faa7a1d
Zambo1998 <37514260+Zakarin1998@users.noreply.github.com>
2023-09-10 00:15:45 +0200

    Update RotatingCube.js

--- app/RotatingCube.js
+++ app/RotatingCube.js
@@ -1,25 +1,25 @@
 import React, { useEffect } from 'react';
-import './RotatingCube.css'; // Import the CSS file for styling
+import styles from './RotatingCube.module.css'; // Import the CSS module for styling
 
 const RotatingCube = () => {
   useEffect(() => {
     const startRotation = () => {
-      const cube = document.querySelector('.cube');
-      cube.classList.add('rotate');
+      const cube = document.querySelector(`.${styles.cube}`); // Use the styles object to reference CSS classes
+      cube.classList.add(styles.rotate); // Use the styles object to reference CSS classes
     };
 
     startRotation();
   }, []);
 
   return (
-    <div className="cube-container">
-      <div className="cube">
-        <div className="face front">THIS IS ICONIC</div>
-        <div className="face back">THIS IS ICONIC</div>
-        <div className="face right">THIS IS ICONIC</div>
-        <div className="face left">THIS IS ICONIC</div>
-        <div className="face top">THIS IS ICONIC</div>
-        <div className="face bottom">THIS IS ICONIC</div>
+    <div className={styles['cube-container']}> {/* Use bracket notation for class names with hyphens */}
+      <div className={styles.cube}>
+        <div className={`${styles.face} ${styles.front}`}>THIS IS ICONIC</div>
+        <div className={`${styles.face} ${styles.back}`}>THIS IS ICONIC</div>
+        <div className={`${styles.face} ${styles.right}`}>THIS IS ICONIC</div>
+        <div className={`${styles.face} ${styles.left}`}>THIS IS ICONIC</div>
+        <div className={`${styles.face} ${styles.top}`}>THIS IS ICONIC</div>
+        <div className={`${styles.face} ${styles.bottom}`}>THIS IS ICONIC</div>
       </div>
     </div>
   );
